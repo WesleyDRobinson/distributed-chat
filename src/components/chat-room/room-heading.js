b3486d13f956cff4f602009f166f8d37de42b885
Wesley Robinson <WesleyDRobinson@users.noreply.github.com>
2018-10-27 15:19:47 -0700

    extract room bootstrap; styles and code quality improvements; (#4)
    * generic renaming
    
    * extract room bootstrap; styles and code quality improvements;

--- src/components/chat-room/room-heading.js
+++ src/components/chat-room/room-heading.js
@@ -12,47 +12,50 @@ class RoomHeading extends HyperHTMLElement {
   }
 
   render() {
-    const container = 'flex justify-around items-center w-100 cf pa2 gradientGO avenir'
-    const peers = 'pointer grow ph2 pv2 br-pill ba b--purple bg-purple near-white tracked tc ttu f7'
-    const titleExit = 'flex justify-center items-center ph3'
+    const container = 'flex justify-around items-center w-100 pa2 gradientGO avenir'
+    const titleAndExit = 'flex justify-center items-center ph3'
     const roomName = 'mv0 mr2 pa2 br1 lh-title f4 f3-ns fw2 near-white bg-black-60'
-    const exit = 'pointer pv2 ph3 br-pill ba b--light-yellow bg-light-yellow gray tracked tc ttu f7'
-    const myId = 'pointer grow  ph2 pv2 br-pill ba b--blue bg-blue near-white tracked tc ttu f7'
+    const headerButton = 'grow pointer ph3 pv2 br-pill ba bg-transparent tc ttu tracked f7 fw6 bg-animate hover-bg-orange hover-near-white outline-transparent'
+    const showPeers = `${headerButton} b--purple purple`
+    const exit = `${headerButton} b--light-yellow bg-light-yellow gray`
+    const myId = `${headerButton} b--blue blue`
+
     return this.html`
-        <div class=${container}>
-          <div class=${peers}
+        <header class=${container}>
+          <button class=${showPeers}
                data-call="showPeers" onClick="${this}" >${getPeerCount()} peers
-          </div>
-          <div class=${titleExit}>
+          </button>
+          <section class=${titleAndExit}>
             <h1 class=${roomName}>${getRoomName()}</h1>
-            <div class=${exit}
+            <button class=${exit}
                  data-call=exit onClick=${this}>exit
-            </div>
-          </div>
-          <div class=${myId}
+            </button>
+          </section>
+          <button class=${myId}
                data-call="showId" onClick="${this}">my id
-          </div>
-        </div>`
+          </button>
+        </header>`
   }
 
-  async showId() {
+  async showId(e) {
     const { id } = await getIpfsNode()
     serveToast(`your peer id is ${id.slice(41)}`)
+    e.target.blur()
   }
 
-  async showPeers() {
+  async showPeers(e) {
     const peers = await getPeers()
     let msg = peers.length === 0
       ? `you are alone in this room`
       : `peers in the room: ${peers.map(peer => peer.slice(41)).join(', ')}`
     serveToast(msg)
+    e.target.blur()
   }
 
-  exit() {
-    leaveRoom()
+  async exit() {
+    await leaveRoom()
     page('/')
   }
-
 }
 
 RoomHeading.define('room-heading')
