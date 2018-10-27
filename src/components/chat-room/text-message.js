44f37f41c7f3e8a7643c15d5a9896674efa16f15
wesley <robinson.wesleyd@gmail.com>
2018-10-27 15:03:05 -0700

    extract room bootstrap; styles and code quality improvements;

--- src/components/chat-room/text-message.js
+++ src/components/chat-room/text-message.js
@@ -1,4 +1,5 @@
 import {
+  serveToast,
   utf8ArrayToStr,
   copyTextToClipboard
 } from './helpers'
@@ -10,24 +11,42 @@ class TextMessage extends HyperHTMLElement {
 
   created() {
     this.message = JSON.parse(this.rawMessage)
-    this.from = this.message.from.slice(41) // .slice(41) is current hacky id
-    this.content = utf8ArrayToStr(this.message.data.data) // wacky structure and encoding shit
-    this.timestamp = new Date().toTimeString() // i hope this is close enough to your actual time; i don't want to import a library to do this any better
+    this.from = this.message.from.slice(41) // .slice(41) is current hacky id system
+    this.content = utf8ArrayToStr(this.message.data.data) // wacky data structure and encoding shit
+    this.timestamp = trim(new Date().toTimeString())
+
+    function trim(time) {
+      // grep timezone from timestring, including parens,
+      // capture group 1 is timezone without parens
+      const re = /\(([^)]+)\)/i
+      const matchGroups = time.match(re);
+      const timezone = matchGroups[1];
+      let abbr = timezone
+      // if timezone contains word-boundary letters, abbreviate it
+      if (timezone.search(/\W/) >= 0) {
+        abbr = timezone
+          .match(/\b\w/g)
+          .join("")
+          .toUpperCase()
+      }
+      return time.replace(re, `(${abbr})`)
+    }
+
     this.render()
   }
 
   render() {
-    const container = 'flex flex-wrap justify-between items-baseline near-black bg-transparent lh-copy slideInUp animated'
+    const container = 'flex flex-wrap justify-between items-baseline near-black bg-transparent lh-copy animated slideInUp'
     const content = 'ph2 pt1 measure measure-wide-ns bg-animate hover-bg-near-white overflow-x-auto f4'
     const spacingDottedLine = "dn db-ns mt1 ml2 mr1 ba b--purple b--dotted bl-0 bt-0 br-0 flex-grow-1 flex-shrink-1 f7"
-    const from = 'ph2 pa2-ns f7 black-60'
-    const timestamp = 'pv1 ph2-ns f6 black-60'
+    const from = 'pr1 pv1 f7 black-60'
+    const timestamp = 'pv1 pl1 f6 black-60'
+
     return this.html`
       <div class=${container}>
         <div class=${content}
               data-call=copyText
-              onClick=${this}>${this.content}
-        </div>
+              onClick=${this}>${this.content}</div>
         <div class=${spacingDottedLine}></div>
         <div class=${from}>from: ${this.from},</div>
         <div class=${timestamp}>${this.timestamp}</div>
@@ -36,7 +55,8 @@ class TextMessage extends HyperHTMLElement {
 
   copyText(e) {
     copyTextToClipboard(e.target.textContent)
-    this.serveToast('copied to clipboard')
+    serveToast('copied to clipboard')
+    e.target.blur()
   }
 }
 
