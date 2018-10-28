98abddaba81330a737c03a978b76230ac31993f9
wesley <robinson.wesleyd@gmail.com>
2018-10-27 22:21:04 -0700

    give structure to broadcast and file upload

--- src/components/chat-room/helpers.js
+++ src/components/chat-room/helpers.js
@@ -1,92 +1,173 @@
-// import ToastAnnounce from '../toast-announce'
+const Room = require('ipfs-pubsub-room')
+const toBuffer = require('blob-to-buffer')
+
 const { wire } = HyperHTMLElement
-const getOrMakeToastContainer = () => {
-  const ipfsRoom = document.querySelector('chat-room')
-  if (ipfsRoom['toast-container']) return ipfsRoom['toast-container']
-  const toastContainer = document.createElement('article')
+
+const bootstrapRoom = function bootstrapRoom({ element, ipfsNode = window.ipfsNode, name = 'default-room-name' }) {
+  // generate a new Room, i.e., subscribe to a topic
+  const room = Room(ipfsNode, name)
+
+  // ** bootstrap room functionality **
+  room.on('peer joined', function peerJoined(peer) {
+    element.incrementPeers()
+    serveToast(`${peer.slice(41)} joined the room`)
+  })
+
+  room.on('peer left', function peerLeft(peer) {
+    element.decrementPeers()
+    serveToast(`${peer.slice(41)} left the room`)
+  })
+
+  room.on('message', function message(message) {
+    const messageEl = wire(message)`<text-message rawMessage=${JSON.stringify(message)}></text-message>`
+    const backboneEl = document.getElementById('messageBackbone')
+    attachMessageToBackbone(messageEl, backboneEl)
+
+    function attachMessageToBackbone(msg, bb) {
+      bb.classList.add('bl', 'bw1', 'b--light-blue')
+      bb.appendChild(msg)
+      bb.scrollTop = bb.scrollHeight
+    }
+  })
+  // ** end room bootstrap **
+
+  return room
+}
+
+const buildContent = {
+  image: async function ({ src, localSrc, message, ...rest }) {
+    return wire()`<img src=${src} alt=${message}>`
+  },
+  text: async function ({ message, ...rest }) {
+    return message
+  },
+}
+
+const copyTextToClipboard = function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text)
+    return
+  }
+
+  navigator.clipboard.writeText(text)
+    .then(() => console.log('navigator clipboard successful!'))
+    .catch(err => console.error('navigator clipboard could not copy text: ', err))
+
+  function fallbackCopyTextToClipboard(text) {
+    const textArea = document.createElement("textarea")
+    textArea.classList.add('clip')
+    textArea.value = text
+    document.body.appendChild(textArea)
+    textArea.focus()
+    textArea.select()
+
+    try {
+      const successful = document.execCommand('copy')
+      const msg = successful ? 'successful' : 'unsuccessful'
+      console.log('text copied via fallback:' + msg)
+    } catch (err) {
+      console.error('unable to copy via fallback', err)
+    }
+
+    document.body.removeChild(textArea)
+  }
+}
+
+const logError = function loadAndReturn(msg, e) {
+  console.error(msg, e)
+  return { msg, err }
+}
+
+const prepareFilesForUpload = async function prepareFilesForUpload(form) {
+  const fileList = form[1].files
+  const item = fileList[0]
+
+  return await packageFile(item)
+
+  function packageFile(file) {
+    return new Promise((resolve, reject) => {
+      const toBufferCallback = function toBufferCallback(err, buffer) {
+        if (err) {
+          console.error(err)
+          reject(err)
+        }
+        resolve([{
+          name: file.name,
+          path: `/files/${file.name}`,
+          buffer,
+        }])
+      }
+      toBuffer(file, toBufferCallback)
+    })
+  }
+}
+
+const getOrMakeToastContainer = async function getOrMakeToastContainer() {
+  let toastContainer = document.getElementById('toast-container')
+  if (toastContainer) return toastContainer
+
+  toastContainer = document.createElement('div')
   toastContainer.id = 'toast-container'
-  toastContainer.className = `z-999 mt5 pa2 fixed top-2 right-1`
-  ipfsRoom.appendChild(toastContainer)
+  toastContainer.className = `fixed top-2 right-1 z-999 mt5 pa2 h-100`
+  document.body.appendChild(toastContainer)
   return toastContainer
 }
-const serveToast = (msg) => {
+
+const serveToast = async function serveToast(msg) {
   const toast = wire()`<toast-announce entry="fadeInDown" exit="fadeOutRight">${msg}</toast-announce>`
-  const toastContainer = getOrMakeToastContainer()
+  const toastContainer = await getOrMakeToastContainer()
   toastContainer.appendChild(toast)
 }
+
+const utf8ArrayToStr = function utf8ArrayToStr(array) {
+  // http://www.onicos.com/staff/iz/amuse/javascript/expert/utf.txt
+  let out, i, len, c;
+  let char2, char3;
+
+  out = "";
+  len = array.length;
+  i = 0;
+  while (i < len) {
+    c = array[i++];
+    switch (c >> 4) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+        // 0xxxxxxx
+        out += String.fromCharCode(c);
+        break;
+      case 12:
+      case 13:
+        // 110x xxxx   10xx xxxx
+        char2 = array[i++];
+        out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+        break;
+      case 14:
+        // 1110 xxxx  10xx xxxx  10xx xxxx
+        char2 = array[i++];
+        char3 = array[i++];
+        out += String.fromCharCode(((c & 0x0F) << 12) |
+          ((char2 & 0x3F) << 6) |
+          ((char3 & 0x3F) << 0));
+        break;
+    }
+  }
+
+  return out;
+}
+
 module.exports = {
-  utf8ArrayToStr: (array) => {
-    // http://www.onicos.com/staff/iz/amuse/javascript/expert/utf.txt
-    let out, i, len, c;
-    let char2, char3;
-
-    out = "";
-    len = array.length;
-    i = 0;
-    while (i < len) {
-      c = array[i++];
-      switch (c >> 4) {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-          // 0xxxxxxx
-          out += String.fromCharCode(c);
-          break;
-        case 12:
-        case 13:
-          // 110x xxxx   10xx xxxx
-          char2 = array[i++];
-          out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-          break;
-        case 14:
-          // 1110 xxxx  10xx xxxx  10xx xxxx
-          char2 = array[i++];
-          char3 = array[i++];
-          out += String.fromCharCode(((c & 0x0F) << 12) |
-            ((char2 & 0x3F) << 6) |
-            ((char3 & 0x3F) << 0));
-          break;
-      }
-    }
-
-    return out;
-  },
-  copyTextToClipboard: (text) => {
-    // for today
-    if (!navigator.clipboard) {
-      fallbackCopyTextToClipboard(text)
-      return
-    }
-
-    // for the future!
-    navigator.clipboard.writeText(text)
-      .then(() => console.log('Async: Copying to clipboard was successful!'))
-      .catch(err => console.error('Async: Could not copy text: ', err))
-
-    function fallbackCopyTextToClipboard(text) {
-      const textArea = document.createElement("textarea")
-      textArea.classList.add('clip')
-      textArea.value = text
-      document.body.appendChild(textArea)
-      textArea.focus()
-      textArea.select()
-
-      try {
-        const successful = document.execCommand('copy')
-        const msg = successful ? 'successful' : 'unsuccessful'
-        console.log('Fallback: Copying text command was ' + msg)
-      } catch (err) {
-        console.error('Fallback: Oops, unable to copy', err)
-      }
-
-      document.body.removeChild(textArea)
-    }
-  },
+  bootstrapRoom,
+  copyTextToClipboard,
+  logError,
   getOrMakeToastContainer,
-  serveToast
+  prepareFilesForUpload,
+  serveToast,
+  buildContent,
+  utf8ArrayToStr,
 }
--- src/components/chat-room/ipfsFunctions.js
+++ src/components/chat-room/ipfsFunctions.js
@@ -1,29 +1,44 @@
-const getRoom = async () => {
+const { buildContent } = require('./helpers')
+
+const getRoom = async function getRoom() {
   const { room } = document.querySelector('chat-room')
   return room ? room : {}
 }
-const getRoomName = async () => {
+const addFile = async function addFile(filePackage) {
+  return window.ipfsNode.files.add(filePackage)
+}
+const broadcastMessage = async function broadcastMessage({ type, ...rest }) {
+  debugger
+  const content = await buildContent[type](rest)
+  debugger
+  const room = await getRoom()
+  debugger
+  return room.broadcast(content)
+}
+const getRoomName = async function getRoomName() {
   const room = await getRoom()
   return room._topic
 }
-const getIpfsNode = async () => {
+const getIpfsNode = async function getIpfsNode() {
   const room = await getRoom()
-  const ipfsNode = room._ipfs.id()
-  return ipfsNode
+  return room._ipfs.id()
 }
-const getPeers = async () => {
-  let room = await getRoom()
-  return room._peers
+const getPeers = async function getPeers() {
+  const room = await getRoom()
+  return room._peers ? room._peers : {}
 }
-const getPeerCount = async () => {
+const getPeerCount = async function getPeerCount() {
   const peers = await getPeers()
   return peers.length
 }
-const leaveRoom = async () => {
+const leaveRoom = async function leaveRoom() {
   const room = await getRoom()
   return room.leave()
 }
+
 module.exports = {
+  addFile,
+  broadcastMessage,
   getRoom,
   getRoomName,
   getIpfsNode,
