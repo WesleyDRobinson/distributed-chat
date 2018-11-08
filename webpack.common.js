353d5e64899fc4629779f0c04df341604786725e
wesley <robinson.wesleyd@gmail.com>
2018-11-07 22:13:57 -0800

    add now.json and more webpack config

---
+++ webpack.common.js
@@ -0,0 +1,9 @@
+'use strict'
+const path = require('path')
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public/js/'),
+        filename: 'app.js'
+    },
+}
