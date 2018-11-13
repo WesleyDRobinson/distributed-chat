8b27e488e2479cf750b76c1dab53b2487cb5dce0
wesley <robinson.wesleyd@gmail.com>
2018-11-12 19:03:45 -0800

    removes cruft

--- src/components/chat-room/helpers/ipfsFunctions.js
+++ src/components/chat-room/helpers/ipfsFunctions.js
@@ -1,5 +1,3 @@
-const { buildContent } = require('./buildContent')
-
 const getRoom = async function getRoom() {
   const { room } = document.querySelector('chat-room')
   return room ? room : {}
@@ -10,12 +8,6 @@ const addFile = async function addFile(filePackage) {
 const broadcastMessage = async function broadcastMessage(message) {
   const room = await getRoom()
   try {
-    // const { type } = message
-    // // build a text-message or image-preview custom element
-    // const content = await buildContent[type](message)
-    // const strContent = JSON.stringify(content)
-
-    // .broadcast() emits the 'message' event, listened for in bootstrapRoom.js
     switch (typeof message) {
       case 'object':
         return room.broadcast(JSON.stringify(message))
